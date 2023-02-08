e537c6be176d38a1825ed52c6318f16c65323384
Alex Hued <66076614+HuedCode88@users.noreply.github.com>
2023-02-07 19:52:25 -0500

    Add files via upload

---
+++ Histogram.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Histogram {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("How many values are in dataset?");
+		int[] dataSet = new int[input.nextInt()];
+		
+		System.out.println("Enter "+dataSet.length+ "values.");
+			for(int i=0;i<dataSet.length;i++) {
+					dataSet[i]=input.nextInt(); 
+			}
+		String h = "*";
+		String empty= "";
+
+			for(int i=0;i<dataSet.length;i++) {
+				for(int j=0;j<dataSet[i];j++) {
+					empty += h;
+					}
+			System.out.println(empty);
+			empty="";
+			}
+	}
+
+}
